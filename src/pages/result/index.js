495229e86548bea96b66a9d65bf04a20e943c980
woo-jk <201702042@o.cnu.ac.kr>
2022-06-07 21:34:58 +0900

    feat: 앨범아트 추천 페이지 제작

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,12 +1,49 @@
 import React from "react";
 import * as Styled from "./styled";
 import * as PageStyled from "../pageStyled";
+// import SampleAlbumArt from "../../images/sample_album_art.jpg";
 
+const albumArtList = [
+  {
+    id: 0,
+    img: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3970/production/_108240741_beatles-abbeyroad-square-reuters-applecorps.jpg.webp",
+  },
+  {
+    id: 1,
+    img: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3970/production/_108240741_beatles-abbeyroad-square-reuters-applecorps.jpg.webp",
+  },
+  {
+    id: 2,
+    img: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3970/production/_108240741_beatles-abbeyroad-square-reuters-applecorps.jpg.webp",
+  },
+  {
+    id: 3,
+    img: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3970/production/_108240741_beatles-abbeyroad-square-reuters-applecorps.jpg.webp",
+  },
+  {
+    id: 4,
+    img: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3970/production/_108240741_beatles-abbeyroad-square-reuters-applecorps.jpg.webp",
+  },
+  {
+    id: 5,
+    img: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3970/production/_108240741_beatles-abbeyroad-square-reuters-applecorps.jpg.webp",
+  },
+];
 function Result() {
   return (
     <PageStyled.Container>
       <Styled.Container>
-        <div>result</div>
+        <Styled.ResultTitle>
+          생성한 <span className="textAA">앨범아트</span> 목록
+        </Styled.ResultTitle>
+        <Styled.AlbumArtListContainer>
+          {albumArtList.map((item) => {
+            return <img className="item_img" src={item.img}></img>;
+          })}
+        </Styled.AlbumArtListContainer>
+        <Styled.ReAnalyze>
+          <input type="button" value="결과 재분석" />
+        </Styled.ReAnalyze>
       </Styled.Container>
     </PageStyled.Container>
   );
--- src/pages/result/styled.js
+++ src/pages/result/styled.js
@@ -1,3 +1,42 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+export const ResultTitle = styled.div`
+  color: white;
+  font-size: 52px;
+  font-weight: 700;
+  margin: 3rem 0 0 4rem;
+
+  .textAA {
+    color: #ff4487;
+  }
+`;
+
+export const AlbumArtListContainer = styled.div`
+  text-align: center;
+  margin-top: 40px;
+  padding: 0 50px 0 50px;
+  .item_img {
+    margin: 0 90px 60px 90px;
+    width: 250px;
+    height: 250px;
+  }
+`;
+
+export const ReAnalyze = styled.div`
+  margin-bottom: 20px;
+  text-align: center;
+  input {
+    cursor: pointer;
+    color: white;
+    font-size: 24px;
+    font-weight: 900;
+    padding: 10px 90px 10px 90px;
+    border-radius: 30px;
+    background: none;
+    border: 3px solid white;
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+`;
